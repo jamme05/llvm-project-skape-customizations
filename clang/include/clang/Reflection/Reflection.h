954535b19111fa66f6f32cad866b4721cdfe2c7f
William Ask Ness <willness@playgroundsquad.com>
2025-08-26 12:31:35 +0200

    Got a baked plugin working

--- clang/include/clang/Reflection/Reflection.h
+++ clang/include/clang/Reflection/Reflection.h
@@ -4,6 +4,16 @@
 
 #include "llvm/ADT/StringRef.h"
 
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Sema/Sema.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace clang;
+
 namespace clang {
   class Decl;
 }
